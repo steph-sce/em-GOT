d315ff42f9146e5f34c14d1ad4a48d6fd3708c28
steph28 <stephane.soucheyre@gmail.com>
2018-06-21 21:44:59 +0200

    Ajout slider page d'accueil

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(document).ready(function () {
+    $('.single-item').slick({
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 8000
+    });
+  });
+});
